80388cc301bfda4d22186c933d6856654729bc6c
Richard Vickerstaff <richardiv.rv@googlemail.com>
2016-04-29 14:58:03 +0100

    Make DB work on mac

--- config/dev.exs
+++ config/dev.exs
@@ -35,8 +35,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :phoenix_webpack, PhoenixWebpack.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "phoenix_webpack_dev",
   hostname: "localhost",
   pool_size: 10
